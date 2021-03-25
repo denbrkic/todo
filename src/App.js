85ef03938c03923d7a4aa5be93e55e3a249d60bb
denbrkic <denbrkic@gmail.com>
2021-03-25 13:48:35 +0100

    setup routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
+import {Route, Link} from 'react-router-dom';
 import './App.scss';
+import Home from './pages/Home/Home';
+import TaskDetails from './pages/TaskDetails/TaskDetails';
 
 const App = () => {
   return (
     <div className="App">
-      Hello
+      <header>
+        <nav>
+          <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/details">Details</Link></li>
+          </ul>
+        </nav>
+      </header>
+      <Route path="/" exact component={Home} />
+      <Route path="/details" exact component={TaskDetails} />
     </div>
   );
 }
---
+++ src/pages/TaskDetails/TaskDetails.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const TaskDetails = () => {
+    return (
+        <h1>Task Details Page</h1>
+    )
+}
+
+export default TaskDetails;
